a2705d593ccc35c72bb578699d1a987df212f2f8
astalberto <albertoherrera205@hotmail.com>
2026-06-09 12:27:25 -0500

    Consulta 01

--- orm/consulta1.py
+++ orm/consulta1.py
@@ -1,2 +1,17 @@
 """
+Titulo - Promedio de edad 
 """
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+
+from modelo import Actor, Serie
+from config import cadena_base_datos
+
+engine = create_engine(cadena_base_datos)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+series = session.query( Serie.titulo,func.avg(Actor.edad).label("promedio_edad")).join(Actor).group_by(Serie).order_by(Serie.titulo).all()
+
+for s in series:
+    print("Titulo : %s - Promedio Edad: %s" % (s.titulo, s.promedio_edad))
