46a689af14922f363e22042d3795f90fadbc6864
astalberto <albertoherrera205@hotmail.com>
2026-06-09 12:47:17 -0500

    Correccion

--- orm/consulta1.py
+++ orm/consulta1.py
@@ -11,7 +11,7 @@ engine = create_engine(cadena_base_datos)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-series = session.query( Serie.titulo,func.avg(Actor.edad).label("promedio_edad")).join(Actor).group_by(Serie).order_by(Serie.titulo).all()
+series = session.query(Serie).order_by(Serie.titulo).all()
 
 for s in series:
-    print("Titulo : %s - Promedio Edad: %s" % (s.titulo, s.promedio_edad))
+    print("Titulo : %s - Promedio Edad: %s" % (s.titulo, s.obtener_edad_actores()))
--- orm/modelo.py
+++ orm/modelo.py
@@ -53,6 +53,16 @@ class Serie(Base):
 
     def __repr__(self):
         return f"Serie: {self.nombre}"
+    
+    def obtener_edad_actores(self):
+        edades = [e.edad for e in self.actores]
+        if len(edades)>0:
+            suma=sum(edades)
+            promedio=suma/len(edades)
+            return promedio
+        else:
+            return 0
+
 
 class Actor(Base):
     __tablename__ = 'actor'
